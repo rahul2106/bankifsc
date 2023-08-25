14619e40d9e225dda32b24c07920ba3b576e2a28
rahul-jodo <95574364+rahul-jodo@users.noreply.github.com>
2023-08-25 18:53:38 +0530

    Second commit

--- bankifsc/settings.py
+++ bankifsc/settings.py
@@ -84,6 +84,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'admin',
+        'PASSWORD': 'MetaApi@7890',
+        'HOST': 'localhost',
+        'POST': '3000'
     }
 }
 
